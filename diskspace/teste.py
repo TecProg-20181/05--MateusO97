8bd6c796cf99cf5d579b5f7e0bcfc3abb8284833
swedge <scott.wedge@gmail.com>
2020-02-11 16:24:14 -0500

    Replace StringIO with io, fix typos ('unnittest')

--- diskspace/teste.py
+++ diskspace/teste.py
@@ -1,13 +1,12 @@
 from diskspace import *
-import unitttest
+import unittest
 import os
-import StringIO
 import subprocess
-import mock
+from unittest import mock
 import io
 import sys
 
-class DiskspaceTests(unnittest.TestCase):
+class DiskspaceTests(unittest.TestCase):
     
     def test_subprocess_check_output(self):
         command = 'du'
@@ -21,7 +20,7 @@ class DiskspaceTests(unnittest.TestCase):
         self.assertEqual('0.00B', result)
 
     def test_print_tree(self):
-        capt = StringIO.StringIO()
+        capt = io.StringIO()
         sys.stdout = capt
 
         print_tree(self.file_tree, self.file_tree_node, self.path,
@@ -33,12 +32,12 @@ class DiskspaceTests(unnittest.TestCase):
 
 
     def test_show_space_list(self):
-        capt = StringIO.StringIO()
+        capt = io.StringIO()
         sys.stdout = capt
 
         print_space_list(self.largest_size, self.file_tree, self.path,
            self.total_size)
 
-         result = "  Size   (%)  File\n6,00Kb 100% /home/mateus/05--MateusO97/diskspace"
+        result = "  Size   (%)  File\n6,00Kb 100% /home/mateus/05--MateusO97/diskspace"
         sys.stdout = sys.__stdout__
         self.assertEqual(result, capt.getvalue())
